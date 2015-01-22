2ba5b860f71bc5c26225730a293c6c0c57518846
Willy Mareka <marekawilly@gmail.com>
2015-01-22 13:00:26 +0300

    Able to submit buildings, but still working on the dropdowns to get from the db

--- application/modules/buildings/controllers/buildings.php
+++ application/modules/buildings/controllers/buildings.php
@@ -6,12 +6,29 @@ class Buildings extends MY_Controller
     {
         // Call the Model constructor
         parent::__construct();
-       
+        $this->load->model("m_buildings");
     }
 
 	function index()
 	{
 		$this->load->view('buildings');
+	}
+
+	function get_estate()
+	{
+         
+	}
+
+	function get_housetype()
+	{
+
+	}
+
+	function add_building()
+	{
+		
+	   $result = $this->m_buildings->enter_building();
+        echo $result;die();
 	}
 
   
--- application/modules/buildings/views/buildings.php
+++ application/modules/buildings/views/buildings.php
@@ -10,9 +10,59 @@
 <body>
 
 
-<div>
-	Real Estate
-</div>
+<div class="content">
+             <form class="form-horizontal group-border-dashed" method="POST"  action ="<?php echo base_url().'buildings/add_building'?>" style="border-radius: 0px;">
+
+              <div class="form-group">
+                <label class="col-sm-3 control-label">Estate</label>
+                <div class="col-sm-6">
+                  <select name="estateid" class="form-control" required>
+                    <option selected="selected" value="NULL">Select the Estate</option>
+                    <option value="1">Rubia</option>  
+                  </select>									
+                </div>
+              </div>
+
+              <div class="form-group">
+                <label class="col-sm-3 control-label">Name</label>
+                <div class="col-sm-6">
+                  <input name="buildingname" type="text" class="form-control" placeholder="Enter Building Name" required>
+                </div>
+              </div>
+    
+              <div class="form-group">
+                <label class="col-sm-3 control-label">Description</label>
+                <div class="col-sm-6">
+                  <textarea name="buildingdescription" class="form-control" placeholder="Enter Building Description" required></textarea>
+                </div>
+              </div>
+
+              <div class="form-group">
+                <label class="col-sm-3 control-label">House Type</label>
+                <div class="col-sm-6">
+                  <select name="housetype" class="form-control" required>
+                    <option selected="selected" value="NULL">Select the House Type</option>
+                    <option value="1">Flat</option>  
+                    <option>3</option>
+                    <option>4</option>
+                    <option>5</option>
+                  </select>									
+                </div>
+              </div>
+
+              <div class="form-group">
+              <div class="col-sm-offset-2 col-sm-10">
+                <button type="submit" class="btn btn-primary">Register</button>
+                <button type="reset" class="btn btn-default">Reset</button>
+              </div>
+              </div>
+
+              <?php 
+                    echo form_close();
+              ?>
+
+            </form>
+          </div>
 
 
 </body>
